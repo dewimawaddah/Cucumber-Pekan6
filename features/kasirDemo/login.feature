2b7399fe7c89c24a1945bd64f8bed6cc4ac067fc
Dewi Mawaddah <dewimawaddahwarrohmah@gmail.com>
2023-02-25 17:49:42 +0700

    positive case login

--- features/kasirDemo/login.feature
+++ features/kasirDemo/login.feature
@@ -3,10 +3,10 @@ Feature: Login Kasir
     Scenario Outline: As a user, I success login
 
         Given I am on the login page
-        When I am input email : "<email>"
-        When I am input password : "<password>"
+        When I am input email "<email>"
+        When I am input password "<password>"
         Then I am click button login
 
     Examples:
-        | email              | password             |
-        | dewitest@gmail.com | 123456               |
+        | email              | password             | 
+        | dewitest@gmail.com | 123456               | 
--- features/step-definitions/stepsKasirDemoLogin.ts
+++ features/step-definitions/stepsKasirDemoLogin.ts
@@ -2,13 +2,17 @@ import { Given, When, Then } from "@wdio/cucumber-framework";
 
 Given(/^I am on the login page$/, async () => {
   await browser.url("/");
-  await browser.pause(20);
+  await browser.pause(10);
 });
 
-When("I am input email: {string}", async (email) => {});
+When("I am input email {string}", async (email) => {
+  await $("#email").setValue(email);
+});
 
-When("I am input password: {string}", async (password) => {});
+When("I am input password {string}", async (password) => {
+  await $("#password").setValue(password);
+});
 
-Then(/^I click button login$/, async () => {
-  await $(".css-1n8i4of").click();
+Then(/^I am click button login$/, async () => {
+  await $(".chakra-button").click();
 });
